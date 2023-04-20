ba84e28232a5ce8d110898ffc9ca803294928048
Johan2809 <johans.palmav@uqvirtual.edu.co>
2023-04-20 01:11:58 -0500

    layoud interfaz aun no funcional

---
+++ ws/agenda/src/controllers/VentanaAgendaController.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import application.Aplicacion;
+
+public class VentanaAgendaController {
+
+	public static void setAplicacion(Aplicacion aplicacion) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
